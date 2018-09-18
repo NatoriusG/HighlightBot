dab31b3b72a6657d67fa543436ed1a831c0d8881
Nathaniel Edgar <nathaniel.edgar.fl@gmail.com>
2018-09-18 18:02:51 -0400

    Updated tag collection logic to be more straightforward and reliable

--- HighlightBot.py
+++ HighlightBot.py
@@ -12,7 +12,7 @@ IMG_HEAD = { 'Authorization': 'Client-ID {}'.format(IMG_ID) }
 IMG_URL = 'https://api.imgur.com/3/album/{}'
 STORE_PATH = 'userdata/{}.nes'
 LOG = 'log.txt'
-LOG_ON = False
+LOG_ON = True
 
 client = Bot(command_prefix=CLIENT_PREFIX)
 
@@ -43,8 +43,14 @@ async def get_tags(link):
 	try:
 		async with client.web_session.get(album_url, headers=IMG_HEAD) as web_response:
 
+			# Get raw data
 			image_data = (await web_response.json())['data']['images'][0]
-			tags = image_data['description'].replace(' ', '').split('#')[1:]
+			description = image_data['description'].split(' ')
+
+			# Verify data and collect tags
+			for substring in description:
+				if substring.startswith('#'):
+					tags.append(substring[1:])
 
 	except:
 		await log('accessing web content failed for link: {}'.format(link), 'fail')
@@ -64,7 +70,7 @@ async def store(username, link):
 		async with aiofiles.open(STORE_PATH.format(username), mode='r') as userdata_file:
 			stored_data = yaml.load(await userdata_file.read())
 	except:
-		await log('Failed to open userdata file for user: {}'.format(username), 'warn')
+		await log('- Failed to open userdata file for user: {}'.format(username), 'warn')
 
 	# Now merge in new data
 	if stored_data == None:
@@ -73,14 +79,13 @@ async def store(username, link):
 		for data_entry in stored_data:
 			# If the new data is a copy of some old data, throw it away
 			if data_entry == register_data:
-				await log('Data found to be duplicate, ignoring', 'warn')
+				await log('- Data found to be duplicate, ignoring', 'warn')
 				register_data = None
 
 	if register_data != None:
 		stored_data.append(register_data)
 
 	merged_data = yaml.dump(stored_data)
-	print('merged data: {}'.format(merged_data))
 
 	# Finally, write to file
 	async with aiofiles.open(STORE_PATH.format(username), mode='w') as userdata_file:
@@ -123,8 +128,10 @@ async def register(*args):
 		result = await store(username, link)
 		
 		if result == 'success':
+			log('new highlight stored successfully')
 			await client.say('New highlight for {} stored successfully.'.format(username))
 		elif result == 'duplicate':
+			log('duplicate highlight ignored')
 			await client.say('Highlight for {} was already stored.'.format(username))
 	else:
 		await log('invalid arguments: [ {}, {} ]'.format(username, link), 'fail')
@@ -142,9 +149,6 @@ async def on_ready():
 			client.users.append(member)
 
 	await log('Setup finished, {} is now online.'.format(client.user.name))
-	await log('ID: {}'.format(client.user.id))
-	await log('Token: {}'.format(TOKEN))
-	await log('Client ID for Imgur: {}'.format(IMG_ID))
 	await log()
 
 client.run(TOKEN)
